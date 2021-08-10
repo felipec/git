ab70840f873c4c19f66241379dc3da9971b6fc6e
Felipe Contreras <felipe.contreras@gmail.com>
2021-08-10 12:10:32 -0500

    stage: add helper to run commands
    Will be useful to run other commands.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- builtin/stage.c
+++ builtin/stage.c
@@ -4,20 +4,33 @@
 
 #include "builtin.h"
 #include "parse-options.h"
+#include "run-command.h"
 
 static const char *const stage_usage[] = {
 	N_("git stage [options] [--] [<pathspec>...]"),
 	NULL
 };
 
+static int rerun(struct child_process *cmd, const char **argv, const char *prefix)
+{
+	strvec_pushv(&cmd->args, argv);
+
+	cmd->dir = prefix;
+	cmd->git_cmd = 1;
+	return run_command(cmd);
+}
+
 int cmd_stage(int argc, const char **argv, const char *prefix)
 {
+	struct child_process cmd = CHILD_PROCESS_INIT;
 	struct option options[] = {
 		OPT_END()
 	};
 
 	argc = parse_options(argc, argv, prefix, options, stage_usage,
-		PARSE_OPT_KEEP_ARGV0 | PARSE_OPT_KEEP_UNKNOWN_OPT | PARSE_OPT_KEEP_DASHDASH);
+		PARSE_OPT_KEEP_UNKNOWN_OPT | PARSE_OPT_KEEP_DASHDASH);
 
-	return cmd_add(argc, argv, prefix);
+	strvec_push(&cmd.args, "add");
+
+	return rerun(&cmd, argv, prefix);
 }
